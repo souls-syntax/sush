9837e63c3096c78403bfe37a7d6e4c9e32b9a13c
Aakarsh Kashyap <souls.syntax@gmail.com>
2026-02-03 19:27:42 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
+  
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+  std::string command;
+  std::getline(std::cin, command);
+  std::cout << command << ": command not found" << "\n";
 }
--- your_program.sh
+++ your_program.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
+# #
+# # Use this script to run your program LOCALLY.
+# #
+# # Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
+# #
+# # Learn more: https://codecrafters.io/program-interface
 #
-# Use this script to run your program LOCALLY.
+# set -e # Exit early if any commands fail
 #
-# Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
+# # Copied from .codecrafters/compile.sh
+# #
+# # - Edit this to change how your program compiles locally
+# # - Edit .codecrafters/compile.sh to change how your program compiles remotely
+# (
+#   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
+#   cmake -B build -S . -G Ninja #-DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+#   cmake --build ./build
+# )
 #
-# Learn more: https://codecrafters.io/program-interface
+# # Copied from .codecrafters/run.sh
+# #
+# # - Edit this to change how your program runs locally
+# # - Edit .codecrafters/run.sh to change how your program runs remotely
+# exec $(dirname "$0")/build/shell "$@"
 
-set -e # Exit early if any commands fail
-
-# Copied from .codecrafters/compile.sh
-#
-# - Edit this to change how your program compiles locally
-# - Edit .codecrafters/compile.sh to change how your program compiles remotely
-(
-  cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
-  cmake --build ./build
-)
-
-# Copied from .codecrafters/run.sh
-#
-# - Edit this to change how your program runs locally
-# - Edit .codecrafters/run.sh to change how your program runs remotely
-exec $(dirname "$0")/build/shell "$@"
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug
