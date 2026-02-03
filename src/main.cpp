8d5c85187d65a395699b54ea26f6cf4e628a7930
Aakarsh Kashyap <souls.syntax@gmail.com>
2026-02-03 19:31:06 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,10 @@ int main() {
   std::cerr << std::unitbuf;
   
   // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
-  std::string command;
-  std::getline(std::cin, command);
-  std::cout << command << ": command not found" << "\n";
+  for(;;) {
+    std::cout << "$ ";
+    std::string command;
+    std::getline(std::cin, command);
+    std::cout << command << ": command not found" << "\n";
+  }
 }
