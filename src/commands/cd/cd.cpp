9d80675819bced00fb78abf7ce78edc8dcd94857
Aakarsh Kashyap <souls.syntax@gmail.com>
2026-02-07 13:15:02 +0530

    codecrafters submit [skip ci]

--- src/commands/cd/cd.cpp
+++ src/commands/cd/cd.cpp
@@ -2,10 +2,19 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <stdlib.h>
 
 void cmd_cd(const std::vector<std::string> &strs) {
+
   std::string s = strs[1];
-  char* path = s.data();
+
+  char* path; 
+  if (s == "~") {
+    path = getenv("HOME");
+  } else {
+    path = s.data();
+  }
+
   int pk = chdir(path);
   if (pk != 0) {
     std::cout << "cd: "<< s <<": No such file or directory\n";
