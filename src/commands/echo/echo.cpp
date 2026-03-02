d85f5254d9308b654d3238c82e0caf8cc5f9c475
Aakarsh Kashyap <souls.syntax@gmail.com>
2026-03-02 17:56:01 +0530

    codecrafters submit [skip ci]

--- src/commands/echo/echo.cpp
+++ src/commands/echo/echo.cpp
@@ -4,6 +4,10 @@
 
 void cmd_echo(const std::vector<std::string> &strs) {
   for(int i = 1; i < strs.size(); i++) {
-    std::cout << strs[i] << " ";
+    std::cout << strs[i];
+    if(i+1 < strs.size()) {
+      std::cout << " ";
+    }
   }
+  std::cout << "\n";
 }
